3e34f4ac215b58a5db20a98577963f0e39f693cc
KTo <kto@lovetou.ru>
2021-07-20 05:40:57 -0700

    function

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,8 +6,13 @@ namespace snake
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.ReadLine();
+			Draw(1, 3, '*');
+		}
+
+		static void Draw(int x, int y, char sym)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
 		}
 	}
 }
